3e487a50d8cecf864a0a6dad6b5612b5e1e474e4
Ignacio <juan.delaiglesia.jdli@gmail.com>
2016-11-28 18:02:13 -0300

    <eliminar puesto> acorde al diagrama de secuencia

--- src/MainPrueba/Main.java
+++ src/MainPrueba/Main.java
@@ -4,16 +4,6 @@
  */
 package MainPrueba;
 
-import Entidades.Competencia;
-import Entidades.Consultor;
-import Gestores.GestorCompetencia;
-import Gestores.GestorConsultor;
-import Gestores.GestorEliminacion;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-
 /**
  *
  * @author Rolando
@@ -21,16 +11,7 @@ import java.util.Date;
 public class Main {
     
     public static void main(String[] args) {
-       ArrayList<Competencia> data = new ArrayList<Competencia>();
-        data= (ArrayList<Competencia>) GestorCompetencia.getInstance().allCompetenciasOrdenadasPorNombre();
-        Competencia competencia= data.get(2);
-    Consultor consultor = GestorConsultor.getInstance().getConsultor();
-                    Date fechaActual = new Date();
-                    DateFormat hourdateFormat = new SimpleDateFormat("HH:mm:ss dd/MM/yyyy");
-                    hourdateFormat.format(fechaActual);
-                    System.out.println("Hora y fecha: " + fechaActual);
-                    GestorEliminacion gestorEliminacion = GestorEliminacion.getInstance();
-                    gestorEliminacion.generarRegistroAuditoria(competencia, consultor, fechaActual);
+       
     
     }
 }
