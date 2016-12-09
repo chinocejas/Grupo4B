3559abfa7150d84596d217f3f3cbef44ba2123c7
Ignacio <juan.delaiglesia.jdli@gmail.com>
2016-12-09 17:33:52 -0300

    funcion CopiarPuesto hasta pregunta (1 parte)

--- src/Gestores/GestorCopia.java
+++ src/Gestores/GestorCopia.java
@@ -11,6 +11,10 @@ import Entidades.Competencia;
 import Entidades.CompetenciaCopia;
 import Entidades.Factor;
 import Entidades.FactorCopia;
+import Entidades.OpcionRespuesta;
+import Entidades.OpcionRespuestaCopia;
+import Entidades.Pregunta;
+import Entidades.PreguntaCopia;
 import Entidades.Puesto;
 import Entidades.PuestoCompetencia;
 import Entidades.PuestoCopia;
@@ -27,14 +31,14 @@ import java.util.Set;
  * @author Eunice
  */
 public class GestorCopia {
-    
+
     private GestorCopia() {
     }
-    
+
     public static GestorCopia getInstance() {
         return GestorCopiaHolder.INSTANCE;
     }
-    
+
     private static class GestorCopiaHolder {
 
         private static final GestorCopia INSTANCE = new GestorCopia();
@@ -44,31 +48,31 @@ public class GestorCopia {
 
         GestorPuesto gestorPuesto = GestorPuesto.getInstance();
         DaoPuesto daoPuesto = new DaoPuesto();
-        DaoCompetencia daoCompetencia= new DaoCompetencia();
+        DaoCompetencia daoCompetencia = new DaoCompetencia();
 
         Date fechaCopia = new Date();
         DateFormat hourdateFormat = new SimpleDateFormat("HH:mm:ss dd/MM/yyyy");
         hourdateFormat.format(fechaCopia);
 
         PuestoCopia puestoCopiado = new PuestoCopia();
-        
+
         puestoCopiado.setPuestoOriginal(puesto);
         puestoCopiado.setNombrePuesto(puesto.getNombrePuesto());
         puestoCopiado.setNombreEmpresa(puesto.getNombreEmpresa());
         puestoCopiado.setDescripcion(puesto.getDescripcion());
         puestoCopiado.setFechaCopia(fechaCopia);
-        
+
         //obtengo el set de COMPETENCIAS originales y lo convierto a list
-        Set competenciasSet=puesto.getCompetencias();
-        List<Competencia> competenciasOriginales=  new ArrayList();
+        Set competenciasSet = puesto.getCompetencias();
+        List<Competencia> competenciasOriginales = new ArrayList();
         competenciasOriginales.addAll(competenciasSet);
-      
+
         PuestoCompetencia puestoCompetencia;
         int idCompetencia;
-        int ponderacion;  
-        
-        List<CompetenciaCopia> competenciasCopia= new ArrayList();
-        
+        int ponderacion;
+
+        List<CompetenciaCopia> competenciasCopia = new ArrayList();
+
         //por cada COMPETENCIA original realizo una copia y la seteo con los parametros correspondientes
         for (Competencia compOriginal : competenciasOriginales) {
 
@@ -98,7 +102,7 @@ public class GestorCopia {
             //obtengo el set de FACTORES originales y lo convierto a list
             Set factoresSet = compOriginal.getFactores();
             List<Factor> factoresOriginales = new ArrayList();
-            factoresOriginales.addAll(factoresSet);//por acaaaa
+            factoresOriginales.addAll(factoresSet);
 
             List<FactorCopia> factoresCopia = new ArrayList();
 
@@ -118,32 +122,93 @@ public class GestorCopia {
                 //seteo la competencia copia a la que pertenece el factor copia
                 factorCopia.setCompetenciaCopia(competenciaCopia);
 
-                //agrego a la lista de factoresCopia el factorCopia recien seteado
-                factoresCopia.add(factorCopia);
+                //---REALIZO LA COPIA DE LAS PREGUNTAS PARA CADA FACTOR--------------
+                //-------------------------------------------------------------------
+                //obtengo el set de PREGUNTAS originales y lo convierto a list
+                Set preguntasSet = factOriginal.getPreguntas();
+                List<Pregunta> preguntasOriginales = new ArrayList();
+                preguntasOriginales.addAll(preguntasSet);
+
+                List<PreguntaCopia> preguntasCopia = new ArrayList();
+System.out.print("tamaño: " + preguntasOriginales.size());
+                //por cada PREGUNTA original realizo una copia y lo seteo con los parametros correspondientes
+                for (Pregunta pregOriginal : preguntasOriginales) {
+
+                    //creo una preguntaCopia
+                    PreguntaCopia preguntaCopia = new PreguntaCopia();
+
+                    //seteo la pregunta copia:
+                    //seteo nombre
+                    preguntaCopia.setNombrePregunta(pregOriginal.getNombrePregunta());
+                    //seteo descripcion
+                    preguntaCopia.setDescripcion(pregOriginal.getDescripcion());
+                    //seteo la pregunta
+                    preguntaCopia.setPregunta(pregOriginal.getPregunta());
+                    //seteo el factor copia al que pertenece la pregunta copia
+                    preguntaCopia.setFactorCopia(factorCopia);
+                    
+                    //seteo opcion de rta:
+                    //pido la opcRta original
+                    OpcionRespuesta opcRtaOriginal=pregOriginal.getOpcionRespuesta();
+                    //creo una OpcionRespuestaCopia
+                    OpcionRespuestaCopia opcRtaCopia= new OpcionRespuestaCopia();
+                    //seteo el nombre de la opcRtaCopia
+                    opcRtaCopia.setNombreOpcionRespuesta(opcRtaOriginal.getNombreOpcionRespuesta());
+                    //seteo la descripcion de la opcRtaCopia
+                    opcRtaCopia.setDescripcion(opcRtaOriginal.getDescripcion());
+                    
+                    /*
+                //obtengo el set de PREGUNTAS dentro de OpcRtaOriginal y lo convierto a list
+                Set preguntasOpcRtaSet = opcRtaOriginal.getPreguntas();
+                List<Pregunta> preguntasOpcRtaOriginales = new ArrayList();
+                preguntasOpcRtaOriginales.addAll(preguntasOpcRtaSet);
+                */
+                
+                    
+                    
+                    //********preguntaCopia.setOpcionRespuestaCopia(opcRtaCopia);
+
+                    //agrego a la lista de preguntasCopia la preguntaCopia recien seteada
+                    preguntasCopia.add(preguntaCopia);
+
+                    
+
+                   
+                }
+                //conversion de list a set
+                    Set preguntasCopiaSet = new HashSet();
+                    preguntasCopiaSet.addAll(preguntasCopia);
+                    factorCopia.setPreguntaCopias(preguntasCopiaSet);
+                    //------------------------------------------------------------------
+                    //------------------------------------------------------------------
+ //agrego a la lista de factoresCopia el factorCopia recien seteado
+                    factoresCopia.add(factorCopia);
+                
+
+                
+
             }
-            //....................................................................
-
             //conversion de list a set
-            Set factoresCopiaSet = new HashSet();
-            factoresCopiaSet.addAll(factoresCopia);
-
-            //seteo los factores a la competencia copiada
-            competenciaCopia.setFactorCopias(factoresCopiaSet);
-
+                Set factoresCopiaSet = new HashSet();
+                factoresCopiaSet.addAll(factoresCopia);
+                //....................................................................
+                
+//seteo los factores a la competencia copiada
+                competenciaCopia.setFactorCopias(factoresCopiaSet);
+                
             //agrego a la lista de competenciasCopia la competenciaCopia inicializada y seteada
-            competenciasCopia.add(competenciaCopia);
+                competenciasCopia.add(competenciaCopia);
+            
 
         }
         //conversion de list a set
-        Set competenciasCopiaSet = new HashSet();
-        competenciasCopiaSet.addAll(competenciasCopia);
+            Set competenciasCopiaSet = new HashSet();
+            competenciasCopiaSet.addAll(competenciasCopia);
 
-        //seteo las competencias al puesto copiado
-        puestoCopiado.setCompetencias(competenciasCopiaSet);
-        //actualizo el puesto ya con todos los datos
-        daoPuesto.savePuestoCopia(puestoCopiado);
-
+            //seteo las competencias al puesto copiado
+            puestoCopiado.setCompetencias(competenciasCopiaSet);
+            //actualizo el puesto ya con todos los datos
+            daoPuesto.savePuestoCopia(puestoCopiado);
         return puestoCopiado;
-            
     }
 }
